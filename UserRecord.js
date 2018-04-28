ac5e27ed6b4543f660a10079224130d69b945328
root <biswasrk@gwmail.gwu.edu>
2018-04-27 20:39:00 -0400

    ui

---
+++ UserRecord.js
@@ -0,0 +1,81 @@
+  function changeSelect()
+{
+
+  
+    $.getJSON("http://localhost:8001/getUserByAddress/" + $("#selectaddress").val(), function(item) {
+       
+      
+            $("#firstname").val(item[0]);
+
+            $("#lastname").val(item[1]);
+
+       });
+}
+
+
+function RegisterUser() {
+
+
+  var firstname = $("#firstname").val();
+
+  var lastname = $("#lastname").val();
+  
+  var id = $("#selectaddress").val();
+  
+  var cost = $("#cost").val();
+
+  var host = $("#hostname").val();
+  
+
+  var url = "http://localhost:8001/RegisterUser/" + firstname + "/" + lastname + "/" + id + "/" + cost + "/" + host;
+
+   $.getJSON(url, function(result) {
+  
+       
+        
+      });
+
+
+ window.location.href = "./UserRecords.html";
+
+};
+
+$(document).ready(function() {
+
+  
+function GetURLParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}
+
+
+  var h = GetURLParameter('host');
+   $("#hostname").val(h);
+
+    //var s = $('<select/>');
+    var s = $("#selectaddress");
+    //s.attr('id', 'dropdown');
+    ddlurl = "http://localhost:8001/getOrganizersList";
+      $.getJSON(ddlurl, function(result){
+          var i = 0;
+            for(var val in result) {
+              
+    $('<option />', {value: result[i], text: result[i]}).appendTo(s);
+      i = i + 1;
+        }
+      s.appendTo('#selectaddressdiv'); 
+
+      });
+
+    
+   
+});
---
+++ UserRecords.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+  $.getJSON("http://localhost:8001/getAllUsers", function(result) {
+  
+      for (var i = 0; i < result.length; i++) {
+       let id = result[i];
+       
+       $.getJSON("http://localhost:8001/getUserByAddress/" + id, function(item) {
+       
+           $('#SuppTable').append('<tr><td>' + item[0] + '</td><td>' + item[1]  + '<td>' + item[4] +'</td>' + '</td>' + '<td>' + item[3] + '</td>'
+
+           	+ '<td><a href="./UserApprovalPage.html?id=' + id  + '&host=' + item[2] + '">Immunize<a/></td>'
+           	
+	+ '<td><a href="./ViewPage.html?id='+ id + "&hash=" + item[3] + '">View<a/></td>'
+      + '<td><a href="http://localhost:8001/unencrypt">Unencrypt</a></td>' 
+           	+ '<td>' +  item[5] +'</td>' 
+
+           	+ '</tr>');
+
+       });
+     
+
+}
+      
+
+    });
+   
+});
